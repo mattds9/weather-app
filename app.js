f4ccf5d0bb353da7276ec2eab1c80ab5cc320e93
mattds9 <mattdeleeuw@hotmail.com>
2019-02-19 17:29:18 -0800

    added formatted address and lat/long, node_modules added to gitignore

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const confi = require('./config.js');
 const request = require('request');
+const yargs = require('yargs');
 
-request({url: `https://maps.googleapis.com/maps/api/geocode/json?key=${confi.config.MY_KEY}&address=%221301%20lombard%20street%20philadelphia%22` , 
+var location = '1301 lombard street philadelphia';
+
+request({url: `https://maps.googleapis.com/maps/api/geocode/json?key=${confi.config.MY_KEY}&address=${location}` , 
 json: true
 }, (error, response, body) => {
         // console.log(JSON.stringify(body, undefined, 2));
         // console.log(JSON.stringify(response, undefined, 2));
         // console.log(JSON.stringify(error, undefined, 2));
+        console.log(body.results[0].formatted_address);
+        console.log(body.results[0].geometry.location);
 });
