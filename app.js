b0b58af905e5bc4e527586728bd7d45f432cd24d
mattds9 <mattdeleeuw@hotmail.com>
2019-02-19 17:06:24 -0800

    prettified the code a little

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const confi = require('./config.js');
 const request = require('request');
 
-request({url: 'https://maps.googleapis.com/maps/api/geocode/json?key=' + confi.config.MY_KEY +'&address=%221301%20lombard%20street%20philadelphia%22' , 
+request({url: `https://maps.googleapis.com/maps/api/geocode/json?key=${confi.config.MY_KEY}&address=%221301%20lombard%20street%20philadelphia%22` , 
 json: true
 }, (error, response, body) => {
-        console.log(JSON.stringify(body, undefined, 4));
+        // console.log(JSON.stringify(body, undefined, 2));
+        // console.log(JSON.stringify(response, undefined, 2));
+        // console.log(JSON.stringify(error, undefined, 2));
 });
